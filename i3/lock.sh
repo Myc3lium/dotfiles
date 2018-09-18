d8d1fb36cb82b512d400392ab75d2d833e37560f
Myc3lium <myc3lium@firemail.cc>
2018-09-18 14:21:26 +0100

    -

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+i3-msg workspace _
+
 scrot ~/Pictures/screen.png
 
 convert ~/Pictures/screen.png -scale 10% -scale 1000% ~/Pictures/screen.png
 convert ~/Picture/screen.png ~/.config/i3/lock.png -gravity center -composite -matte ~/Pictures/screen.png
 pactl set-sink-mute 0 1
 
-i3lock -e -u -f -d -n -i ~/Pictures/screen.png ## -n options stops forking, so commands afterwards
+i3lock -e -f -d -n -i ~/Pictures/screen.png ## -n options stops forking, so commands afterwards
                                                ## aren't executed until unlock
 pactl set-sink-mute 0 0
 rm ~/Pictures/screen.png
+
+i3-msg workspace back_and_forth
 
