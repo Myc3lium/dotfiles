d0abd7ab97d1455978441494d6307a09c382d8e6
Myc3lium <myc3lium@firemail.cc>
2018-09-25 21:16:52 +0100

    Did some refactoring

--- i3/pyvol.py
+++
@@ -1,4 +0,0 @@
-from sys import stdin
-
-string = ('▁▂▃▄▅▆▇█' [:int(stdin.read()[:-2]) // 10])
-print(string + ('_' * (8 - len(string))))
--- i3/wttr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "$(curl wttr.in/Stafford?0T)" | python3 -c "
-from sys import stdin;
-lines = [line [15 :].rstrip().lstrip() for line in stdin.read().split('\n') [2 :-1]]
-print(' '.join(lines))
-"
-
