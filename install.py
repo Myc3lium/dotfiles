0d888a7161d513385e5e50556d13ee81a7ac0296
Myc3lium <myc3lium@firemail.cc>
2018-09-26 18:01:45 +0100

    -

--- install.py
+++ install.py
@@ -4,9 +4,12 @@ cp i3/ $HOME/.config/i3
 cp .bin $HOME/.bin
 cp .calcurse $HOME/.calcurse
 
+cp .gitconfig $HOME/.gitconfig
 cp .vimrc $HOME/.vimrc
+cp  rc.conf $HOME/.config/ranger/rc.conf
 cp .bashrc $HOME/.bashrc
 cp .psrc $HOME/.psrc
 cp .Xresources $HOME/.Xresources
 cp .xinitrc $HOME/.xinitrc
 cp .Xmodmap $HOME/.Xmodmap
+cp .tmux.conf $HOME/.tmux.conf
